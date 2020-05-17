1463fc7d054d3ae3b7af6bec98e3dc31eccdc17c
yunlanli <chnsidney@gmail.com>
2020-05-17 05:02:42 -0400

    Use Array of objects instead of integers for drawing on canvas

--- frontend/src/scripts/visualizeSorting.js
+++ frontend/src/scripts/visualizeSorting.js
@@ -3,12 +3,18 @@
 * array of random integers
 * @param size: the size of the array of random integer
 */
-function initializeCanvaArray(size){
+export default function initializeCanvaArray(size,canvas){
+
+    // initialize canvas
+    canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
+
+    // prepare array for drawing
     var array = createRandomArray(size);
-    drawArray(array);
+    transformArrayFormat(array,canvas.width,canvas.height,"rgba(0,0,200,0.3");
+
+    // draw array on the canvas node
+    drawArray(array,canvas);
 }
-
-
 
 /* 
 * Find the html canvas element and creates a visualization of
@@ -16,25 +22,18 @@ function initializeCanvaArray(size){
 * for each element in the input array
 * @param array: the array to draw on the canvas element
 */
-function drawArray(array) {
-  var canvas = document.getElementById("canvas");
+function drawArray(array,canvas) {
+  // var canvas = document.getElementById("canvas");
   if (canvas.getContext) {
-    var ctx = canvas.getContext("2d");
-    
+    var ctx = canvas.getContext('2d');
+
     // iterate over the elements in the random array
     for (let key in array){
-        // coords of rectangle
-      let space = 2;
-      let height = array[key];
-      let width = Math.floor(canvas.width/array.length)-space;
-      let xStart = key*(width+space);
-      let yStart = 150-height;
+      const current = array[key]
       
-      console.log(height);
-      
-        // draw rectangle
-      ctx.fillStyle = "rgba(0,0,200,0.5)";
-      ctx.fillRect(xStart,yStart,width,height);
+      // draw rectangle
+      ctx.fillStyle = current.color;
+      ctx.fillRect(current.x,current.y,current.width,current.height);
     }
   }
 }
@@ -42,8 +41,33 @@ function drawArray(array) {
 /* 
 * Generates an array of integers of random size
 * @param size: the desired size of the integer array
-* @return an array object of ints
+* @return an array of objects
 */
 function createRandomArray(size){
-return Array(size).fill().map(() => Math.floor(Math.random()*size*10));
+  return Array(size).fill().map(() => {
+    return {value: Math.floor(Math.random()*size*10)}
+  });
+}
+
+function transformArrayFormat(array, width, height, color){
+  // width of each element based on array size and canvas width
+  const WIDTH = Math.floor(width/array.length);
+  const SPACE = 0.1*WIDTH;
+  
+  function getHeight(val){
+    var largest = 10*array.length;
+
+    return Math.floor(val*(height/largest));
+  }
+
+  // iterate over elements in the array and set attributes necessary for drawing
+  for (let key in array){
+    let current = array[key];
+
+    current.width = WIDTH - SPACE;
+    current.height = getHeight(current.value);
+    current.x = key*WIDTH + SPACE;
+    current.y = height - current.height;
+    current.color = color;   
+  }
 }
